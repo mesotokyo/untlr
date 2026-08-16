35f51e89f0012c39340741974c029cd3230c18eb
mesotokyo <meso@meso.tokyo>
2026-08-17 00:16:50 +0900

    support live-reloading

--- lib/untlr/server.py
+++ lib/untlr/server.py
@@ -3,11 +3,66 @@ import http.server
 import tomllib
 import logging
 import json
+from http import HTTPStatus
+from wsgiref.types import WSGIEnvironment, StartResponse
+import time
+import email.utils
 
 from .renderer import render
 from .variable_manager import VariableManager
 
 logger = logging.getLogger(__name__)
+
+class ServerApp:
+    config: ClassVar[dict[str, Any]] = {}
+    environ: WSGIEnvironment
+    start_response: StartResponse
+
+    def __init__(self, environ: WSGIEnvironment, start_response: StartResponse):
+        self.environ = environ
+        self.start_response = start_response
+
+    def date_time_string(self, timestamp=None):
+        if timestamp is None:
+            timestamp = time.time()
+        return email.utils.formatdate(timestamp, usegmt=True)
+        
+    def send(self, status:HTTPStatus, content: str = "") -> bytes:
+        if content:
+            resp = content.encode("utf-8")
+        else:
+            resp = f"{status.phrase}".encode("utf-8")
+        response_headers = [
+            ('Content-type', 'text/html'),
+            ("Content-Length", str(len(resp))),
+            ("Last-Modified", self.date_time_string())            
+        ]
+        self.start_response(f"{status.value} {status.phrase}", response_headers)
+        return resp
+
+    def __iter__(self):
+        method = self.environ["REQUEST_METHOD"]
+        path = self.environ["PATH_INFO"]
+
+        if method != "GET":
+            yield self.send(HTTPStatus(404))
+            return
+        
+        vm = VariableManager(self.config)
+        vars = vm.generate_for_path(path)
+        if "Posts" in vars:
+            logger.debug(json.dumps(vars["Posts"][0], indent=2, ensure_ascii=False))
+
+        html = render(self.config, vars).encode("utf-8")
+
+        status = '200 OK'
+        response_headers = [
+            ('Content-type', 'text/html'),
+            ("Content-Length", str(len(html))),
+            ("Last-Modified", self.date_time_string())            
+        ]
+        self.start_response(status, response_headers)
+        yield html
 
 class TestServer(http.server.BaseHTTPRequestHandler):
     config: ClassVar[dict[str, Any]] = {}
--- untlr_server.py
+++ untlr_server.py
@@ -1,17 +1,19 @@
+import re
 import os
 import sys
 import tomllib
-import http
+import http.server
 import argparse
 from pathlib import Path
 import logging
-from typing import Protocol
+from typing import Any
 
 sys.path.append(os.path.abspath("lib"))
 sys.path.append(os.path.abspath(os.path.join("depends", "jinja-importprops", "src")))
 sys.path.append(os.path.abspath(os.path.join("depends", "npf-renderer", "src")))
 
-from untlr.server import TestServer
+from untlr.server import TestServer, ServerApp
+import livereload
                 
 logger = logging.getLogger("untlr_server")
 
@@ -35,6 +37,37 @@ def get_parser() -> argparse.ArgumentParser:
                         action="store_true")
     return parser
 
+def check_excluded(filename: str, excludes: list[re.Pattern]) -> bool:
+    basename = os.path.basename(filename)
+    for p in excludes:
+        if p.match(basename):
+            return True
+    return False
+
+def start_live_server(config: dict[str, Any]):
+    ignores: list[str] = config.get("watch", {}).get("ignore", [])
+    ignore_re: list[re.Pattern] = []
+    for t in ignores:
+        ignore_re.append(re.compile(t))
+
+    ServerApp.config.update(config)
+    server = livereload.Server(ServerApp)
+    if "theme_file" in config:
+        server.watch(config["theme_file"])
+    if "theme_dir" in config:
+        server.watch(config["theme_dir"], ignore=lambda x:check_excluded(x, ignore_re))
+    host = config["system"]["listen"]
+    port = config["system"]["port"]
+    server.serve(port=port, host=host)
+
+def start_server(config: dict[str, Any]):
+    TestServer.config.update(config)
+    listen = (config["system"]["listen"], config["system"]["port"])
+    httpd = http.server.HTTPServer(listen, TestServer)
+    logger.info(f"start server on {listen}")
+    httpd.serve_forever()
+
+
 def main():
     parser = get_parser()
     args = parser.parse_args(namespace=Arguments())
@@ -47,26 +80,27 @@ def main():
     # load config file
     try:
         with open(args.config, "rb") as fp:
-            TestServer.config = tomllib.load(fp)
+            config = tomllib.load(fp)
     except IOError:
         logger.critical("no config file given")
         sys.exit(-1)
-        
+
     if args.theme_file:
         if args.theme_file.is_file():
-            TestServer.config["theme_file"] = args.theme_file
+            config["theme_file"] = args.theme_file
         if args.theme_file.is_dir():
-            TestServer.config["theme_dir"] = args.theme_file
+            config["theme_dir"] = args.theme_file
 
-    if (not "theme_file" in TestServer.config
-        and not "theme_dir" in TestServer.config):
+    if (not "theme_file" in config
+        and not "theme_dir" in config):
         logger.critical("theme file or directory not given or invalid file or directory is given")
         sys.exit(-1)
 
-    listen = (TestServer.config["system"]["listen"], TestServer.config["system"]["port"])
-    httpd = http.server.HTTPServer(listen, TestServer)
-    logger.info(f"start server on {listen}")
-    httpd.serve_forever()
+    if config.get("watch", {}).get("enabled", False):
+        start_live_server(config)
+    else:
+        start_server(config)
+        
 
 if __name__ == "__main__":
     main()
