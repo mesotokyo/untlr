69d6861307f0c663492c156fef8e91c0ad373194
mesotokyo <meso@meso.tokyo>
2026-08-21 02:45:25 +0900

    implement Tumblr client with cache

--- lib/untlr/tumblr_client.py
+++ lib/untlr/tumblr_client.py
@@ -2,33 +2,75 @@ import urllib.parse
 import urllib.request
 import json
 from typing import Any
+import logging
 
-API_HOST = "https://api.tumblr.com"
-BLOG_ID = "mesotokyo.tumblr.com"
-API_PATH = f"/v2/blog/{BLOG_ID}/posts"
-API_KEY = "9jzeTeOst7drVpT11bfcBXDQgC8p9BjbpSzGdzZhc9nI7k9sJt"
+logger = logging.getLogger(__name__)
 
-def get_recent_posts() -> bytes|dict[str, Any]:
-    param: dict[str, str|int] = {
-        "api_key": API_KEY,
-        "limit": 10,
-        #"npf": True
-    }
-    qs = urllib.parse.urlencode(param)
-    url: str = f"{API_HOST}{API_PATH}?{qs}"
+type Config = dict[str, Any]
+class InvalidResponseError(Exception):
+    pass
 
-    #print(url)
-    with urllib.request.urlopen(url) as req:
-        ctype: str|None = req.headers.get("Content-Type")
-        if ctype is not None and ctype.startswith("application/json"):
-            raw_result = req.read()
-            result = json.loads(raw_result)
-            return result
-
-    return req.read()
+class TumblrClient:
+    _api_key: str
+    _blog_id : str
+    _api_path: str
     
+    def __init__(self, config: Config):
+        try:
+            self._api_key = config["api"]["key"]
+            host = config["api"]["host"]
+            blog_id = config["blog"]["id"]
+        except KeyError as e:
+            msg = f"config key not found: {e}"
+            raise KeyError(msg)
+        self._api_path = f"{host}/v2/blog/{blog_id}"
+
+    def get_post(self, post_id: int) -> dict[str, Any]:
+        param: dict[str, str|int] = {
+            "api_key": self._api_key,
+            "id": post_id,
+            "npf": True
+        }
+        qs = urllib.parse.urlencode(param)
+        url: str = f"{self._api_path}/posts?{qs}"
+        return self._get_request(url)
+
+    def get_recent_posts(self, limit: int = 10, offset: int = 0) -> dict[str, Any]:
+        param: dict[str, str|int] = {
+            "api_key": self._api_key,
+            "limit": limit,
+            "offset": offset,
+            "npf": True
+        }
+        qs = urllib.parse.urlencode(param)
+        url: str = f"{self._api_path}/posts?{qs}"
+        return self._get_request(url)
+
+    def _get_request(self, url: str) -> dict[str, Any]:
+        logger.debug(f'request url: {url}')
+        with urllib.request.urlopen(url) as req:
+            ctype: str|None = req.headers.get("Content-Type")
+            raw_result = req.read()
+            if ctype is not None and ctype.startswith("application/json"):
+                result = json.loads(raw_result)
+                return result
+
+        msg = f"invalid response: {raw_result}"
+        raise InvalidResponseError(msg)
+    
+
 if __name__ == "__main__":
-    d = get_recent_posts()
+    config = {
+        "api": {
+            "key": "9jzeTeOst7drVpT11bfcBXDQgC8p9BjbpSzGdzZhc9nI7k9sJt",
+            "host": "https://api.tumblr.com"
+        },
+        "blog": {
+            "id": "mesotokyo.tumblr.com"
+        }
+    }
+    c = TumblrClient(config)
+    d = c.get_recent_posts()
     if isinstance(d, dict):
         print(json.dumps(d, ensure_ascii=False, indent=2))
     else:
--- lib/untlr/variable_manager.py
+++ lib/untlr/variable_manager.py
@@ -1,24 +1,109 @@
 from typing import Any
 import re
 import json
+from pathlib import Path
+import logging
+logger = logging.getLogger(__name__)
 
-from .posts_response import PostsResponse
+from .posts_response import PostsResponse, Post
 from .tumblr_theme_parser import escape_identifier
+from .tumblr_client import TumblrClient
+
+class CachedClient:
+    cache_enabled: bool
+    cache_dir: Path | None
+
+    def __init__(self, config):
+        self.config = config
+
+        try:
+            self.cache_enabled = config["system"]["cache"]
+            self.cache_dir = Path(config["system"]["cache_dir"])
+        except KeyError:
+            self.cache_enabled = False
+            self.cache_dir = None
+
+        self._ensure_cache_dir()
+
+    def _get_from_cache(self, cache_name: str) -> dict[str, Any]:
+        if self.cache_dir is None:
+            msg = "cache_dir not set"
+            raise FileNotFoundError(msg)
+        fn = self.cache_dir / f"{cache_name}.json"
+        with fn.open("rt", encoding="utf-8") as fp:
+            data = json.load(fp)
+        return data
+
+    def _ensure_cache_dir(self):
+        if not self.cache_dir:
+            msg = "cache_dir is not set"
+            raise FileNotFoundError(msg)
+        if not self.cache_dir.exists():
+            self.cache_dir.mkdir()
+            return
+        if self.cache_dir.is_dir():
+            return
+        msg = f'"{self.cache_dir}" is not a directory'
+        raise NotADirectoryError(msg)
+
+    def _save_cache(self, cache_name: str, data: dict[str, Any]):
+        if self.cache_dir is None:
+            msg = "cache_dir not set"
+            raise FileNotFoundError(msg)
+        fn = self.cache_dir / f"{cache_name}.json"
+        with fn.open("wt", encoding="utf-8") as fp:
+            json.dump(data, fp, indent=2)
+        
+    def get_posts(self, offset: int, limit: int) -> dict[str, Any]:
+        fname = f"posts_{offset}_{limit}"
+        if self.cache_enabled:
+            try:
+                data = self._get_from_cache(fname)
+                return data
+            except FileNotFoundError as e:
+                logger.info(f"{e}: cache is not available")
+
+        c = TumblrClient(self.config)
+        data = c.get_recent_posts(limit, offset)
+
+        if self.cache_enabled:
+            try:
+                self._save_cache(fname, data)
+            except FileNotFoundError as e:
+                logger.info(f"{e}: cache is not available")
+        return data
+
+    def get_post(self, post_id: int) -> dict[str, Any]:
+        fname = f"post_{post_id}"
+        if self.cache_enabled:
+            try:
+                data = self._get_from_cache(fname)
+                return data
+            except FileNotFoundError as e:
+                logger.info(f"{e}: cache is not available")
+
+        c = TumblrClient(self.config)
+        data = c.get_post(post_id)
+
+        if self.cache_enabled:
+            try:
+                self._save_cache(fname, data)
+            except FileNotFoundError as e:
+                logger.info(f"{e}: cache is not available")
+        return data
+        
 
 class VariableManager:
     """manage variables"""
-    pr: PostsResponse
+    #pr: PostsResponse
     config: dict[str, Any]
+    post_per_page: int
+    client: CachedClient
     
     def __init__(self, config: dict[str, Any]):
-        #source = "posts.json"
-        #source = "post_npf.json"
         self.config = config
-
-        post_cache = f"posts{config["system"]["cache_file_postfix"]}"
-        with open(post_cache, "rt", encoding="utf-8") as fp:
-            data = json.load(fp)
-        self.pr = PostsResponse(data)
+        self.post_per_page = config["system"].get("post_per_page", 10)
+        self.client = CachedClient(config)
 
     def _load_additional_content(self, vars: dict[str, Any], page_type: str):
         section = f"{page_type}_page"
@@ -38,40 +123,83 @@ class VariableManager:
 
             key = f"_{c_type}_"
             vars[key]  = content
-    
-    def generate_for_path(self, path: str) -> dict[str, Any]:
-        vars: dict[str, Any] = {}
-        vars.update(self.pr.blog.to_variables())
 
+    def _set_default_values(self, vars: dict[str, Any]):
         # import default values with escaping
         def_vars = self.config["default_values"]
         for k in def_vars:
             key = escape_identifier(k)
             vars[key]  = def_vars[k]
-        #vars.update(self.config["default_values"])
+            
+    def generate_for_path(self, path: str) -> dict[str, Any]:
+        vars: dict[str, Any] = {}
 
         if path == "/":
             self._generate_for_index(vars)
+            self._set_default_values(vars)
             self._load_additional_content(vars, "index")
-                
+
+        if path.startswith("/page/"):
+            m = re.match(r"/page/(\d+)/?", path)
+            if m:
+                page_num = m.group(1)
+                self._generate_for_page(page_num, vars)
+            self._set_default_values(vars)
+            self._load_additional_content(vars, "post")
+            
         if path.startswith("/post/"):
             m = re.match(r"/post/(\d+)/?", path)
             if m:
                 post_id = m.group(1)
                 self._generate_for_post(post_id, vars)
+            self._set_default_values(vars)
             self._load_additional_content(vars, "post")
 
         return vars
-        
+    
     def _generate_for_index(self, vars: dict[str, Any]):
-        vars["Posts"] = [p.to_variables("index") for p in self.pr.posts]
-        vars["IndexPage"] = True
+        self._generate_for_page("1", vars)
+       
+    def _generate_for_page(self, page_num: str, vars: dict[str, Any]):
+        try:
+            page = int(page_num)
+        except ValueError:
+            page = 0
+        if page < 1:
+            logger.error(f"invalid page number: {page_num}")
+            return 
+
+        offset = self.post_per_page * (page-1)
+        data = self.client.get_posts(offset, self.post_per_page)
+        pr = PostsResponse(data)
+
+        vars.update(pr.blog.to_variables())
+        d = {
+            "Posts": [p.to_variables("index") for p in pr.posts],
+            "IndexPage": True,
+            "Pagination": True,
+            "NextPage":  f"/page/{page+1}",
+            "CurrentPage": page,
+            "TotalPages": "28",
+        }
+        if page > 2:
+            d["PreviousPage"] = f"/page/{page-1}"
+
+        vars.update(d)
 
     def _generate_for_post(self, post_id: str, vars: dict[str, Any]):
-        # find post
-        for p in self.pr.posts:
-            if p.post_id == post_id:
-                vars["Posts"] = [p.to_variables("post")]
-                break
+        try:
+            the_id = int(post_id)
+        except ValueError:
+            the_id = 0
+
+        if the_id == 0:
+            logger.error(f"invalid page number: {post_id}")
+            return 
+
+        c = TumblrClient(self.config)
+        data = self.client.get_post(the_id)
+        pr = PostsResponse(data)
+        vars["Posts"] = [pr.posts[0].to_variables("post")]
         vars["IndexPage"] = False
     
